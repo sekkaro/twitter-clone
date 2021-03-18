935513e4fd833ad526d7c06f846fd73c43006921
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2021-03-19 00:41:01 +0900

    react authentication - login & signup

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,35 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 import React from "react";
 import "./App.css";
 import Users from "./components/Users";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "./components/Landing";
+import { setContext } from "apollo-link-context";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
-const client = new ApolloClient({
+const httpLink = new HttpLink({
   uri: "http://localhost:4000",
+});
+const authLink = setContext(async (req, { headers }) => {
+  const token = localStorage.getItem("token");
+
+  return {
+    ...headers,
+    headers: {
+      Authorization: token ? `Bearer ${token}` : null,
+    },
+  };
+});
+
+const link = authLink.concat(httpLink as any);
+const client = new ApolloClient({
+  link: link as any,
   cache: new InMemoryCache(),
 });
 
@@ -17,6 +40,12 @@ function App() {
         <Switch>
           <Route path="/landing">
             <Landing />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/login">
+            <Login />
           </Route>
           <Route path="/">
             <Users />
